623f826dddd01bb35be0035e0bf0feb57d2505c5
Jedla18 <m.jedlicka2@post.cz>
2026-05-14 14:01:24 +0200

    update

--- database.py
+++ database.py
@@ -3,6 +3,10 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = "postgresql://jed0061:pswjed0061@158.196.134.95:5433/pill_dispenser_postgres"
 
+#lokalni db v případě potřeby odkomentovat a zakomentovat tu vzdálenou
+#DATABASE_URL = "sqlite:///./pill_dispenser.db"
+
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
